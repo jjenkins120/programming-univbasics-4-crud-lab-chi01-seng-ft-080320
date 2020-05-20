0900a8cb7a38be9f94780450135d6f72211e2a03
Jeff Jenkins <jjenkins120@gmail.com>
2020-05-20 19:53:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,7 +36,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  indexed_array = ["wow", "what", "am", "i"]
+  indexed_array[0]
 end
 
 def retrieve_last_element_from_array(array)
