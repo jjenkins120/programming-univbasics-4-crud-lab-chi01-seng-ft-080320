7d4559b728b85514ef76dfb48d2b4517f998d35a
Jeff Jenkins <jjenkins120@gmail.com>
2020-05-20 19:45:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,9 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  grocery_list = ["cheese", "avocado", "bread", "bacon"]
+  grocery_list.unshift("arrays!")
+  p grocery_list
 end
 
 def remove_element_from_end_of_array(array)
