b7357aae97e2c6f99b3236261fc6238fe357f1b7
Jeff Jenkins <jjenkins120@gmail.com>
2020-05-20 19:49:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,9 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  grocery_list = ["wow", "avocado", "bread", "bacon", "arrays!"]
+  wow_array = grocery_list.shift 
+  p wow_array
 end
 
 def retrieve_element_from_index(array, index_number)
